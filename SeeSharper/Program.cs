99aec65fe3403690062c45efd9ce75ff3d962603
fullmetalcache <fullmetalcache@gmail.com>
2018-12-27 09:42:09 -0700

    Added comments to Program.cs

--- SeeSharper/Program.cs
+++ SeeSharper/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * File: Program.cs
+ * Author: Brian Fehrman (fullmetalcache)
+ * Date: 2018-12-27
+ * Description:
+ *      This is the main entroy point for the SeeSharper program. 
+ * 
+ */
+
+using System;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -8,9 +17,10 @@ namespace SeeSharper
     {
         static void Main(string[] args)
         {
+            //Create a new WebShot object for screenshotting.
             WebShot webshot = new WebShot(10);
 
-
+            //Some examples of screenshotting for now...
             webshot.ScreenShot("https://google.com");
             webshot.ScreenShot("https://www.blackhillsinfosec.com");
             webshot.ScreenShot("http://www.reddit.com");
